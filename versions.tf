e7a316f71a0497d3e3de6a701bef4f841961fbce
Benjamin Jouyait <benjamin.jouyait@skale-5.com>
2023-12-23 19:51:17 +0100

    build: Add local terraform provider

--- versions.tf
+++ versions.tf
@@ -18,6 +18,10 @@ terraform {
       source  = "alekc/kubectl"
       version = ">= 2.0.4"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.4.1"
+    }
     null = {
       source  = "hashicorp/null"
       version = "3.2.2"
